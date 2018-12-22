4992720c5c75cbb748f4e304f965336d0d56e654
Санёк Виничук <sashavinichuk@gmail.com>
2018-12-22 10:38:45 +0200

    AndroidListActivitys 1.6.5

--- app/src/main/java/com/example/student/androidlistactivitys/Humen.java
+++ app/src/main/java/com/example/student/androidlistactivitys/Humen.java
@@ -1,5 +1,60 @@
 package com.example.student.androidlistactivitys;
 
-public class Humen {
 
+
+public class Humen {
+    private String Name;
+    private String Sername;
+    private int Age;
+    private int Height;
+    private int Weight;
+
+    public Humen(String Name, String Sername, int Age, int Height, int Weight)
+    {
+        this.Name = Name;
+        this.Sername = Sername;
+        this.Age = Age;
+        this.Height = Height;
+        this.Weight = Weight;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getSername() {
+        return Sername;
+    }
+
+    public void setSername(String sername) {
+        Sername = sername;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+
+    public int getHeight() {
+        return Height;
+    }
+
+    public void setHeight(int height) {
+        Height = height;
+    }
+
+    public int getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(int weight) {
+        Weight = weight;
+    }
 }
