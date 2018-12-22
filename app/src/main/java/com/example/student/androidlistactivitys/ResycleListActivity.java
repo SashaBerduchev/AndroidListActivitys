1628ddaa65b1a6b4365a3bafdd769444b9b00720
Санёк Виничук <sashavinichuk@gmail.com>
2018-12-22 09:37:30 +0200

    AndroidListActivitys 1.6.4

--- app/src/main/java/com/example/student/androidlistactivitys/ResycleListActivity.java
+++ app/src/main/java/com/example/student/androidlistactivitys/ResycleListActivity.java
@@ -2,12 +2,29 @@ package com.example.student.androidlistactivitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResycleListActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private TextView textname, textsername, textage, textheight, textweight;
+    private Button btnhuman;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resycle_list);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        textname = findViewById(R.id.TextName);
+        textsername = findViewById(R.id.TextSername);
+        textage = findViewById(R.id.TextAge);
+        textheight = findViewById(R.id.TextHeight);
+        textweight = findViewById(R.id.TextWeight);
+        btnhuman = findViewById(R.id.btnHuman);
+
+
+
     }
 }
