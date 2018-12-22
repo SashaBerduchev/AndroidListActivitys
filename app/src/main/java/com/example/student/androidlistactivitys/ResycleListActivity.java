10b8e3d011ec9cedb5c7db268e5ac85b132aa739
Санёк Виничук <sashavinichuk@gmail.com>
2018-12-22 09:05:26 +0200

    AndroidListActivitys 1.6.3

---
+++ app/src/main/java/com/example/student/androidlistactivitys/ResycleListActivity.java
@@ -0,0 +1,13 @@
+package com.example.student.androidlistactivitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResycleListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resycle_list);
+    }
+}
