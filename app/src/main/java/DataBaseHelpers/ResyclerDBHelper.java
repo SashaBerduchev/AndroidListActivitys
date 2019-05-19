d9a6a60bc16e3edcb3f44f11c458cd843e59044a
Sasha <sashaberduchev@gmail.com>
2019-05-19 20:12:57 +0300

    ResycleListActivity.java ProdDataDBHelper.java ProdDataDBHelper.java

--- app/src/main/java/DataBaseHelpers/ResyclerDBHelper.java
+++ app/src/main/java/DataBaseHelpers/ResyclerDBHelper.java
@@ -6,29 +6,39 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class ProdDataDBHelper extends SQLiteOpenHelper {
-    public static final String DATABASE_NAME="DbProdactions";
-    public static final int DATABASE_VERSION=3;
-    public static final String TABLE = "DbProdaction";
+public class ResyclerDBHelper extends SQLiteOpenHelper {
+
+    public static final String DATABASE_NAME="DbResycler";
+    public static final int DATABASE_VERSION=1;
+    public static final String TABLE = "DbResTable";
     public static final String _ID = "id";
     public static final String COLUMN_1="Name";
-    public static final String COLUMN_2="Model";
+    public static final String COLUMN_2="Sername";
+    public static final String COLUMN_3="Age";
+    public static final String COLUMN_4="Height";
+    public static final String COLUMN_5="Weight";
     public static final String CREATE_TABLE="CREATE TABLE "
             + TABLE + " ( "
             + _ID + " INTEGER PRIMARY KEY, "
             + COLUMN_1 + " TEXT, "
-            + COLUMN_2 + " TEXT)";
+            + COLUMN_2 + " TEXT, "
+            + COLUMN_3 + " TEXT, "
+            + COLUMN_4 + " TEXT, "
+            + COLUMN_5 + " TEXT)";
 
-       public ProdDataDBHelper(Context context) {
+    public ResyclerDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public void addData(String s1,String s2)
+    public void addData(String s1,String s2, String s3, String s4, String s5)
     {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(COLUMN_1,s1);
         values.put(COLUMN_2,s2);
+        values.put(COLUMN_3,s3);
+        values.put(COLUMN_4,s4);
+        values.put(COLUMN_5,s5);
         db.insert(TABLE,null,values);
         db.close();
     }
@@ -36,7 +46,7 @@ public class ProdDataDBHelper extends SQLiteOpenHelper {
     public Cursor fetchData()
     {
         SQLiteDatabase db = this.getReadableDatabase();
-        String str = ("SELECT rowid _id, * FROM DbProdaction");
+        String str = ("SELECT rowid _id, * FROM DbResTable");
         return db.rawQuery(str,null);
 
     }
@@ -50,4 +60,5 @@ public class ProdDataDBHelper extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE);
         onCreate(db);
     }
+
 }
--- app/src/main/java/com/example/student/androidlistactivitys/ResycleListActivity.java
+++ app/src/main/java/com/example/student/androidlistactivitys/ResycleListActivity.java
@@ -1,5 +1,6 @@
 package com.example.student.androidlistactivitys;
 
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,11 +11,14 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import DataBaseHelpers.ResyclerDBHelper;
+
 public class ResycleListActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private TextView textname, textsername, textage, textheight, textweight;
     private Button btnhuman;
+    private ResyclerDBHelper resyclerDBHelper;
     private RecyclerView.Adapter recycleViewAdapter;
     private  RecyclerView.LayoutManager layoutManager;
     private ArrayList<Humen> list = new ArrayList<>();
@@ -22,6 +26,7 @@ public class ResycleListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resycle_list);
+        resyclerDBHelper = new ResyclerDBHelper(this);
 
         recyclerView = findViewById(R.id.recyclerView);
         textname = findViewById(R.id.TextName);
@@ -32,21 +37,24 @@ public class ResycleListActivity extends AppCompatActivity {
         btnhuman = findViewById(R.id.btnHuman);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
+        list.add(new Humen(resyclerDBHelper.COLUMN_1, resyclerDBHelper.COLUMN_2, resyclerDBHelper.COLUMN_3, resyclerDBHelper.COLUMN_4, resyclerDBHelper.COLUMN_5));
 
+        recycleViewAdapter = new RecyclerViewAdapter(list);
+        recyclerView.setAdapter(recycleViewAdapter);
         btnhuman.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Cursor cursor = resyclerDBHelper.fetchData();
                 String name = textname.getText().toString();
                 String sername =  textsername.getText().toString();
                 String age = String.valueOf(textage.getText());
                 String heigt = String.valueOf(textheight.getText());
                 String weight = String.valueOf(textweight.getText());
-                list.add(new Humen(name, sername, age, heigt, weight));
+                resyclerDBHelper.addData(name, sername, age, heigt, weight);
+                list.add(new Humen(resyclerDBHelper.COLUMN_1, resyclerDBHelper.COLUMN_2, resyclerDBHelper.COLUMN_3, resyclerDBHelper.COLUMN_4, resyclerDBHelper.COLUMN_5));
 
                 recycleViewAdapter = new RecyclerViewAdapter(list);
                 recyclerView.setAdapter(recycleViewAdapter);
-
-
 
             }
         });
